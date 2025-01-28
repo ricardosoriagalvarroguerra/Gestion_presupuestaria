91f9ad071f401284878a64aa343b3f8898adf263
ricardosoriagalvarroguerra <110310949+ricardosoriagalvarroguerra@users.noreply.github.com>
2025-01-28 08:00:38 -0400

    Update centralizador-ppt.py

--- centralizador-ppt.py
+++ centralizador-ppt.py
@@ -488,7 +488,7 @@ def main():
             cookie_expiry_days=config["cookie"]["expiry_days"]
         )
 
-        nombre, auth_status, username = authenticator.login("Iniciar Sesión", "main")
+        nombre, auth_status, username = authenticator.login("Iniciar Sesión", location="main")
 
         if auth_status is False:
             st.error("Usuario o contraseña incorrectos.")
